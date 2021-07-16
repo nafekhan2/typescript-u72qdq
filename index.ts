1761f94da0c5bc46d48b3640a78f6d740478a3e5
nafekhan2 <nafekhan2@gmail.com>
2021-07-16 15:19:18 +0530

    literal type and type added

--- index.ts
+++ index.ts
@@ -5,16 +5,39 @@ import './style.css';
 const appDiv: HTMLElement = document.getElementById('app');
 appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
 
-const Person={
-  name:string;
-  age:number;
-  role:[number,string];
-}={
-  name:'nafees',
-  age:37,
-  role:[1,"Admin"]
+enum Role {
+  ADMIN = 'Admin',
+  AUTHOR = 'AUTHOR'
 }
 
-console.log(Person.name);
+const Person = {
+  name: 'nafees',
+  age: 20,
+  hobbies: ['sports', 'cricket']
+};
 
+console.log(Person.age);
+const res = Add(4, 50, true, 'as-number');
+console.log('res=' + res);
 
+type combinetype = number | string;
+function Add(
+  n1: combinetype,
+  n2: combinetype,
+  showResult: boolean,
+  resultConversion: 'as-number' | 'as-text'
+) {
+  if (
+    typeof n1 === 'number' &&
+    typeof n2 === 'number' &&
+    resultConversion === 'as-number'
+  ) {
+    return n1 + n2;
+  }
+  const result = n1.toString() + n2.toString();
+  if (showResult) {
+    console.log('Result = ' + result);
+  } else {
+    return result;
+  }
+}
